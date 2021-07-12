1edd512447a43a4655e9eb9580ccfe6438c9bb06
Nathan silva <email.do.natann@gmail.com>
2021-07-12 19:31:42 -0300

    12/07

---
+++ w3Schools/12/07.js
@@ -0,0 +1,12 @@
+// function isPower(n) {
+
+//     for (let i = 1; i < n + 1; i++) {
+//         for (let j = 2; j < n + 1; j++) {
+//             const result = i ** j
+//             if (result === n) return true
+//         }
+//     }
+//     return false
+// }
+// console.log(isPower(256));
+
